54a8db4acf0cc5f798ace0bf9c65a700f83002eb
Adam Tang <adamtang@gmail.com>
2022-09-02 15:42:07 -0700

    removed comment

--- public/index.js
+++ public/index.js
@@ -5,7 +5,6 @@ const initializePage = () => {
     const container = document.createElement('section');
     container.classList = 'container flex-column';
     document.body.appendChild(container);
-    // console.log(container);
 };
 
 window.onload = () => {
